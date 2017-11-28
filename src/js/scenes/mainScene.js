a0319a477124a5431c8f907043fdee198605db26
Jake Huxell <darkduckmovies@gmail.com>
2017-11-28 01:33:02 +0000

    Draw a cat on the screen?

--- src/js/scenes/mainScene.js
+++ src/js/scenes/mainScene.js
@@ -1,8 +1,27 @@
-
 const MainScene = {
-		sceneName_Main : "Main"
+		sceneName_Main : "Main",
+		
+		scene_assets : {
+				"sprites": {
+						
+						"../../sprites/PlaceholderCat.png": {
+								tile: 140,
+								tileh: 140,
+
+								map: {
+										cat_normal: [0, 0],
+										cat_chatty: [0, 1]
+								}
+						}
+				}
+		}
 };
 
-Crafty.defineScene(MainScene.sceneName_Main, function(){
+Crafty.defineScene(MainScene.sceneName_Main, function(menuData){
 		Crafty.background("#000");
+		
+		Crafty.load(MainScene.scene_assets, function(){
+				let cat = Crafty.e("2D", "Canvas", "cat_normal")
+						.attr({x:10, y:10});
+		});
 });

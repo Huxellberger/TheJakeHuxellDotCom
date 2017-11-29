d511ec6a445f7c5b6cb125c251ad167067c48da0
Jake Huxell <darkduckmovies@gmail.com>
2017-11-29 18:06:34 +0000

    Horrible cat thing

--- src/js/scenes/mainScene.js
+++ src/js/scenes/mainScene.js
@@ -4,24 +4,30 @@ const MainScene = {
 		scene_assets : {
 				"sprites": {
 						
-						"../../cat_sprite.png": {
-								tile: 140,
-								tileh: 140,
+						"http://bin.smwcentral.net/u/15240/cat_sprite.png": {
+								tile: 240,
+								tileh: 240,
 
 								map: {
 										cat_normal: [0, 0],
 										cat_chatty: [0, 1]
 								}
 						}
+				},
+
+				"audio" : {
+						"cat_chatter" : ["../../audio/cat_chat.wav"]
 				}
 		}
 };
 
 Crafty.defineScene(MainScene.sceneName_Main, function(menuData){
-		Crafty.background("#000");
+		Crafty.background("brown");
 		
 		Crafty.load(MainScene.scene_assets, function(){
-				var actual_size = Crafty.e("2D, Canvas, cat_normal")
+				let cat = Crafty.e("2D, Canvas, cat_normal, Chatter")
 						.attr({x: 10, y:10});
+
+				cat.chatterSound = "cat_chatter";
 		});
 });
